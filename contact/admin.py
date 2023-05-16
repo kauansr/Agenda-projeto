477cd8c6f9692fb0cfee6ac9d31479bd2f664d57
kauansr <kakagamerbr3003@gmail.com>
2023-05-16 14:06:28 -0300

    Projeto agenda feita totalmente em python com framework django

---
+++ contact/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Contact, Category
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show',)
+    ordering = ('id',)
+    search_fields = ('id', 'first_name', 'last_name',)
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = ('first_name', 'last_name', 'show',)
+    list_display_links = ('id', 'phone',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('id',)
---
+++ contact/forms.py
@@ -0,0 +1,156 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Contact
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
+
+
+class ContactForm(forms.ModelForm):
+
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+
+        ), required=False,
+    )
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Primeiro nome'
+        }), help_text='Seu primeiro nome'
+    )
+
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone',
+                  'email', 'description', 'category', 'picture',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        phone = cleaned_data.get('phone')
+
+        return super().clean()
+
+
+class RegisterForm(UserCreationForm):
+
+    first_name = forms.CharField(
+        required=True, min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True, min_length=3,
+    )
+
+    email = forms.EmailField(
+        required=True, min_length=3,
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email',
+                  'username', 'password1', 'password2',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', ValidationError(
+                'E-mail existente', code='invalid'))
+
+        return email
+
+
+class RegisterUpdateForm(forms.ModelForm):
+
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length': 'Please, add more than 2 letters.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text='Use the same password as before.',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name',
+                  'email', 'username', 'password1', 'password2',)
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+
+        user = super().save(commit=False)
+
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2', ValidationError('Senhas nao batem'))
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+
+        if current_email != email:
+
+            if User.objects.filter(email=email).exists():
+                self.add_error('Email', ValidationError(
+                    'E-mail existente', code='invalid'))
+
+        return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+
+            except ValidationError as errors:
+                self.add_error('password1', ValidationError(errors))
+
+        return password1
